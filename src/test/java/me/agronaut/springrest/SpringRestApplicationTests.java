b070bad14d8a9ae29c01854f29fd05cc1bb7114d
Gabor Horvath <gaborka812@gmail.com>
2022-09-03 16:07:38 +0200

    action v3

--- src/test/java/me/agronaut/springrest/SpringRestApplicationTests.java
+++ src/test/java/me/agronaut/springrest/SpringRestApplicationTests.java
@@ -2,8 +2,10 @@ package me.agronaut.springrest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
+@ContextConfiguration(classes=SpringRestApplication.class)
 class SpringRestApplicationTests {
 
     @Test
--- src/test/java/me/agronaut/springrest/UserServiceTests.java
+++ src/test/java/me/agronaut/springrest/UserServiceTests.java
@@ -3,6 +3,8 @@ package me.agronaut.springrest;
 
 import me.agronaut.springrest.Model.User;
 import me.agronaut.springrest.Service.UserService;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackage;
@@ -17,12 +19,13 @@ import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @ContextConfiguration(classes = {UserService.class})
 @AutoConfigurationPackage
-@SpringBootTest
 public class UserServiceTests {
     @Autowired
     UserService userSD;
     // private static final Logger log = LoggerFactory.getLogger(UserServiceTests.class);
+
     @Test
+    @Order(1)
     public void testRegister() {
         User testUser = new User();
         testUser.setUsername("testUser");
@@ -35,7 +38,16 @@ public class UserServiceTests {
     }
 
     @Test
+    @Order(2)
     public void testLogin() {
+        User InserTestUser = new User();
+        InserTestUser.setUsername("testUser");
+        InserTestUser.setPassword("testUser");
+        InserTestUser.setEmail("test@user.com");
+
+        Long insertedId = userSD.save(InserTestUser).getId();
+
+
         User testUser = new User();
         testUser.setUsername("testUser");
         testUser.setPassword("testUser");
