f9ab7d84eb5da14346d2d4c2c739c5c779a55dd5
Gabor Horvath <gaborka812@gmail.com>
2023-05-25 15:40:54 +0200

    fix npm sonarqube

--- React/rest-api/sonarqube-scanner.js
+++ React/rest-api/sonarqube-scanner.js
@@ -3,7 +3,7 @@ scanner(
     {
         serverUrl: "http://192.168.2.55:9002",
         options: {
-            "sonar.sources": "./src",
+            "sonar.sources": "./React/rest-api/src",
             "sonar.token": "sqa_56e90bb26a71c833ced6d7eea8b424be1c87e49e",
             "sonar.projectKey": "springReact",
             "sonar.projectName": "springReact"
